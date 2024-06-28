fd1d66bc913eba4fd6c046b44fcf72a06c31a4c9
Джамиля Эркенова <DM.Erkenova@ispras.ru>
2024-06-28 21:39:48 +0300

    updated (exuse my security breach)

--- site/app.py
+++ site/app.py
@@ -1,20 +1,20 @@
 from elasticsearch import Elasticsearch
 from flask import Flask, render_template, request
 import json
+from getpass import getpass
 
 app = Flask(__name__)
 
-
 client = Elasticsearch(
   "https://f935605f13ee463382d108587978eec1.us-central1.gcp.cloud.es.io:443",
-  api_key="N1U3bFdaQUJTZkUza2t5UjcxNy06NjRJY3k1XzNScy1tN0VjcGJEaVM4Zw=="
+  api_key="TUJqa1g1QUJ5TnR1Sm5meHg1UDU6OHRZTzBFWTFTeUNDa2ViLVo5MjVnZw=="
 )
 
-with open('db\spans.json', 'r', encoding='utf-8') as f:
-    documents = json.load(f)
+# with open('db\spans.json', 'r', encoding='utf-8') as f:
+#   documents = json.load(f)
 
 # for i, doc in enumerate(documents):
-#   client.index(index="test-index", id=i, document=doc)
+#   client.index(index="onegin", id=i, document=doc)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -27,15 +27,15 @@ def home():
         }
         body = {
                 "query": {
-                  "term": {
+                  "match": {
                     search_types[index]: q
                   }
                 }
               }
-        res = client.search(index='test-index', body=body)
-        return render_template('base.html', res=res['hits']['hits'])
+        res = client.search(index='onegin', body=body)
+        return render_template('base.html', res=res['hits']['hits'], search_query=q, selected_index=index)
     else:
-        return render_template('base.html', res={})
+        return render_template('base.html', res={}, search_query='', selected_index='')
 
 if __name__ == '__main__':
     app.run()
