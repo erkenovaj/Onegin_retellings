2b68d533b3c8d5093faf5ce8bd204e4939482c63
Джамиля Эркенова <DM.Erkenova@ispras.ru>
2024-06-28 19:39:35 +0300

    updated db

--- site/app.py
+++ site/app.py
@@ -6,51 +6,15 @@ app = Flask(__name__)
 
 
 client = Elasticsearch(
-  "https://ff50edc582264643a4b2cd2ea5b4ba22.us-central1.gcp.cloud.es.io:443",
-  api_key="VUpUVkpwQUJyVGNGVkJoWHBJSkE6aUtKa3pMZHdRS0N5WXpPZ0t5SUdDQQ=="
+  "https://f935605f13ee463382d108587978eec1.us-central1.gcp.cloud.es.io:443",
+  api_key="N1U3bFdaQUJTZkUza2t5UjcxNy06NjRJY3k1XzNScy1tN0VjcGJEaVM4Zw=="
 )
 
-documents = [
-  { "index": { "_index": "index_name", "_id": "7"}},
-  {"_id": "ObjectId('6673513eac128b59cf8c5cda')",
-  "span": " Евгения Онегина.",
-  "label_name": "full_name",
-  "left_context": "И он расстроенный уходит, на этом наш роман кончается и что вы об этом думаете, как бы сами делайте выводы, как говорится, вот, спасибо.",
-  "right_context": "В начале романа Евгений Онегин нам предстает частым гостем богемской тусовки Питера.",
-  "sentence": "Пересказ Евгения Онегина.",
-  "sentence_id": 1,
-  "text_id": "ObjectId('6673513eac128b59cf8c5cd9')"},
-  { "index": { "_index": "index_name", "_id": "8"}},
-  {
-  "span": " точнее",
-  "label_name": "slang",
-  "left_context": "Он ходит на все балы, на все обеды, на все представления в театре, то есть он уже там как свой.",
-  "sentence": "Онегина пересказ",
-  "right_context": "Для него женщины это просто ачивки, которые он хочет завоевать.",
-  "text_id": "ObjectId('6673513eac128b59cf8c5cd9')",
-  "sentence_id": 4},
-  { "index": { "_index": "index_name", "_id": "9"}},
-  {  "span": " присыпал",
-  "label_name": "slang",
-  "left_context": "Также у него были достаточно поверхностные знания, но этих поверхностных знаний ему было достаточно, чтобы в обществе он казался умным.",
-  "right_context": "И из этого получился, получился вид таких глубоких начитанных знаний.",
-  "sentence": "Точнее, эти поверхностные знания он также присыпал немножко харизмой и потом сверху чуть-чуть обаянием.",
-  "text_id": "ObjectId('6673513eac128b59cf8c5cd9')",
-  "sentence_id": 12},
-  { "index": { "_index": "index_name", "_id": "10"}},
-  {
-  "span": " у него случился кризис четверти века.",
-  "label_name": "interesting",
-  "left_context": "И из этого получился, получился вид таких глубоких начитанных знаний.",
-  "right_context": "И теперь он не понимает, что ему делать дальше.",
-  "sentence": "Но, к сожалению, к своим 26 годам он понял, что все это ему надоело, а точнее у него случился кризис четверти века.",
-  "text_id": "ObjectId('6673513eac128b59cf8c5cd9')",
-  "sentence_id": 14}
-]
+with open('db\spans.json', 'r', encoding='utf-8') as f:
+    documents = json.load(f)
 
-
-client.bulk(operations=documents)
-print(documents)
+# for i, doc in enumerate(documents):
+#   client.index(index="test-index", id=i, document=doc)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -68,7 +32,7 @@ def home():
                   }
                 }
               }
-        res = client.search(index='index_name', body=body)
+        res = client.search(index='test-index', body=body)
         return render_template('base.html', res=res['hits']['hits'])
     else:
         return render_template('base.html', res={})
